08050b28de48eb6eed5ad66fa4bb60ac54b5cd32
Amir Vahedi <amirvahedix@gmail.com>
2021-03-31 13:07:30 +0430

    Store workouts data in local storage

--- script.js
+++ script.js
@@ -73,7 +73,13 @@ class App {
     #workouts = [];
 
     constructor() {
+        // Get user's position
         this._getPosition();
+
+        // Get data from local storage
+        this._getLocalStorage();
+
+        // Attach event handlers
         form.addEventListener('submit', this._newWorkout.bind(this));
         inputType.addEventListener('change', this._toggleElavationField);
         containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
@@ -102,7 +108,13 @@ class App {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(this.#map);
 
+        // Handling clicks on the map
         this.#map.on('click', this._showForm.bind(this));
+
+
+        this.#workouts.forEach(work => {
+            this._renderWorkoutMarker(work)
+        })
     }
 
     _showForm(mapE) {
@@ -163,6 +175,8 @@ class App {
         // Hide form + clear input fields
         this._hideForm();
 
+        // Set local storage to all workouts
+        this._setLocalStorage()
     }
 
     _renderWorkoutMarker(workout) {
@@ -257,6 +271,28 @@ class App {
         setTimeout(() => {
             form.style.display = 'grid'
         }, 1000);
+    }
+
+    _setLocalStorage() {
+        localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+    }
+
+    _getLocalStorage() {
+        const data = JSON.parse(localStorage.getItem('workouts'))
+
+        if (!data) return;
+
+        this.#workouts = data;
+
+        this.#workouts.forEach(work => {
+            this._renderWorkout(work);
+            this._renderWorkoutMarker(work)
+        })
+    }
+
+    reset() {
+        localStorage.removeItem('workouts');
+        location.reload()
     }
 }
 
